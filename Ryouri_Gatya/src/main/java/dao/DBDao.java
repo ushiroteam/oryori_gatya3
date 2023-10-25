d9a834968daa90df75425fa87a595d9b0f192108
0801JA <0801JA@DESKTOP-P1KJOL0>
2023-10-25 09:13:11 +0900

    データベースを追加するDao作成しました

---
+++ Ryouri_Gatya/src/main/java/dao/DBDao.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.GachaDB;
+import manager.DBmanager;
+
+public class DBDao {
+	
+	public static List<GachaDB> importGacha(){
+		
+		Connection connection = null;
+		ResultSet resultSet = null;
+		List<GachaDB> list = new ArrayList<>();
+		String SQL ="INSET INTO category? VALUES ( ?, ? );";
+		try {
+			connection = DBmanager.getConnection();
+			
+			try (PreparedStatement statement= connection.prepareStatement(SQL)){
+				resultSet = statement.executeQuery();
+				
+				resultSet.next();
+			
+				GachaDB db = new GachaDB();
+					statement.setString(0, "");
+					statement.setString(1, "");
+				
+			
+			list.add(db);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}finally {
+			
+		}
+		
+		
+		return list;
+	}
+}
